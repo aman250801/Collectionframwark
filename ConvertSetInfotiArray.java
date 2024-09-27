249d0ada5f3ae0d38e179be8ebab1aa88fe96eb4
aman250801 <amansen0781@gmail.com>
2024-09-27 18:39:39 +0530

    Add files via upload

---
+++ ConvertSetInfotiArray.java
@@ -0,0 +1,12 @@
+package com.setInterfaceEx;
+
+public class ConvertSetInfotiArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//declaration
+		HashSet<Integer> h1=new HashSet<>();
+
+	}
+
+}
