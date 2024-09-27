b9fe79ad43c332216858d38ff1b409c46ce2d842
aman250801 <amansen0781@gmail.com>
2024-09-27 18:38:19 +0530

    Add files via upload

---
+++ StringEx.java
@@ -0,0 +1,32 @@
+package com.listInterfaceDemo;
+
+import java.util.ArrayList;
+
+public class StringEx {
+	
+	public static void main(String[] args) {
+		//declare
+		ArrayList<String> a1=new ArrayList<>();
+		
+		a1.add("Indore");
+		a1.add("Bhopal");
+		a1.add("Vidisha");
+		a1.add("Ujjain");
+		a1.add("Basoda");
+		System.out.println(a1);
+		System.out.println("Size of list:-"+a1.size());
+		//a1.remove(2);
+		//System.out.println(a1);
+		//retrivale
+		//System.out.println(a1.get(2));
+		//replace
+		//a1.set(2, "");
+		//System.out.println(a1);
+		//System.out.println(a1.contains(400));
+		//for(Object o : a1) {
+			//System.out.println("value of list:-"+0);
+		}
+		
+	}
+
+
