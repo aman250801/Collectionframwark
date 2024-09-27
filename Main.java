b4faf9c9d04900ed90b7692dfb06bbc31a61d06c
aman250801 <amansen0781@gmail.com>
2024-09-27 18:41:28 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
-package com.genericsDemo;
+package com.comparableAndCompatorDemo;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Employe> e1 = new ArrayList<Employe>();
+		
+		//add
+		e1.add(new Employe(101,"Aman", 55000));
+		e1.add(new Employe(101,"Vishal", 35000));
+		e1.add(new Employe(101,"Jaddu", 45000));
+		
+		//sorting
+		Collections.sort(e1);
+		
+		for(Employe obj :e1) {
+			System.out.println(obj.id+" "+obj.name+" "+obj.sal);
+		}
+ 	}
 
 }
